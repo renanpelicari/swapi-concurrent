84f299aa64ebf2b69ab6b0d3f17dd643e81bfb50
Renan Peliçari <renanpr@ciandt.com>
2018-05-05 17:16:53 -0300

    first commit

---
+++ src/main/java/rpr/poc/swapiconcurrent/SwapiConcurrentApplication.java
@@ -0,0 +1,12 @@
+package rpr.poc.swapiconcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiConcurrentApplication.class, args);
+	}
+}
